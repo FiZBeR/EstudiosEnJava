3ae78be317c7c81371366040aced95c39e115274
cristian <cristiancv2000@hotmail.com>
2022-11-28 23:17:39 -0500

    28/11/2022

---
+++ src/TipoString/EjemploString.java
@@ -0,0 +1,2 @@
+package TipoString;public class EjemploString {
+}
---
+++ src/Variables/Variables.java
@@ -0,0 +1,22 @@
+public class Variables {
+
+    public static void main(String[] args){
+        String hola = "Hola mundo";
+        System.out.println();
+        /*sout = System.out.print*/
+        System.out.println("hola = " + hola.toUpperCase());
+
+        int numeroEntero = 10;
+        float numeroFlotante = 10.1;
+        String cadenaLetras = "hola";
+
+        System.out.println("cadenaLetras = " + cadenaLetras + "numeroEntero" + numeroEntero
+                +"numeroFlotante" + numeroFlotante);
+
+        boolean valor = true
+
+                
+
+    }
+
+}
